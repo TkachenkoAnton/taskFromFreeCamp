bd8bb74b4dc83f3c8cd300be315aacc93cfe531e
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-10-20 21:54:43 +0300

    добавляет частичное решение задачи

---
+++ freeCamp.js
@@ -0,0 +1,37 @@
+// Setup
+var collection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+const updateRecords = function (object, id, prop, value) {
+
+  if (!object[id].hasOwnProperty(prop)) {
+    object[id][prop] = value;
+  } 
+  if (value === '') {
+    delete object[id][prop]
+  }
+  if (object[id][prop] === [] && object[id].hasOwnProperty(prop)){
+    object[id][prop].push(value)
+  }
+  return object;
+  
+}
+
+console.log(updateRecords(collection, 5439, 'artist', 'ABBA'));
